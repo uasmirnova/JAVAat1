286d7257e0dbb4a33b4d63ab20f3a3985efbe0af
uasmirnova <uasmirnova21@yandex.ru>
2022-09-27 16:17:40 +0300

    test api added

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -2,6 +2,7 @@ package ru.netology.service;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 
 public class CashbackHackServiceTest {
 
@@ -32,5 +33,32 @@ public class CashbackHackServiceTest {
         int actual = service.remain(1100);
 
         Assert.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void ShouldReturn100_2() {
+
+        int expected = 100;
+        int actual = service.remain(900);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void ShouldReturn0_2() {
+
+        int expected = 0;
+        int actual = service.remain(1000);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void ShouldReturn900_2() {
+
+        int expected = 900;
+        int actual = service.remain(1100);
+
+        Assertions.assertEquals(expected, actual);
     }
 }
